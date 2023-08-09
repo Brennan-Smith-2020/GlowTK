87c3f974b42b431c220b6f47d5d97bcaf5cff61c
AnimGUI <104740023+PackageCreator@users.noreply.github.com>
2023-08-08 23:09:47 -0400

    All files for the project (currently)

---
+++ GlowTK/__init__.py
@@ -0,0 +1,5 @@
+import tkinter as tk 
+from tkinter import filedialog
+import logging
+import os
+from GlowTK.src.main.wrapper import promptFileOpen
---
+++ GlowTK/src/main/wrapper.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from tkinter import filedialog
+
+def create_window(Title, Geometry, Background):
+    window = tk.Tk()
+    window.title(Title)
+    window.geometry(Geometry)
+    if Background == "default":
+        pass
+    else:
+        window.configure(bg=Background)
+    return window
+def add_button(window, text, command, X, Y):
+    button = tk.Button(window, text=text, command=command)
+    button.place(x=X, y=Y)
+
+def promptFileOpen(Title):
+    file_path = filedialog.askopenfilename(title=Title)
+    return file_path
