57dbaf5b036a2ecf0f976d76fd0ea6f2b1d4572e
AnimGUI <104740023+PackageCreator@users.noreply.github.com>
2023-08-08 23:38:45 -0400

    Added func to add labels

--- GlowTK/src/main/wrapper.py
+++ GlowTK/src/main/wrapper.py
@@ -13,6 +13,10 @@ def create_window(Title, Geometry, Background):
 def add_button(window, text, command, X, Y):
     button = tk.Button(window, text=text, command=command)
     button.place(x=X, y=Y)
+    
+def add_label(window, text, X, Y):
+    label = tk.Label(window, text)
+    label.place(x=X, y=Y)
 
 def promptFileOpen(Title):
     file_path = filedialog.askopenfilename(title=Title)
