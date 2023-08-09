0d9966644531c4c62bb47b1fdbedde515269e475
AnimGUI <104740023+PackageCreator@users.noreply.github.com>
2023-08-09 02:27:50 -0400

    Added colors :D

--- GlowTK/src/main/wrapper.py
+++ GlowTK/src/main/wrapper.py
@@ -1,6 +1,16 @@
 import tkinter as tk
 from tkinter import filedialog
 
+
+class color:
+    WHITE = "#ffffff"
+    BLACK = "#0f0f0f"
+    GREY = "#615d53"
+    
+
+def initialize():
+    color = color()
+
 def create_window(Title, Geometry, Background):
     window = tk.Tk()
     window.title(Title)
@@ -10,18 +20,21 @@ def create_window(Title, Geometry, Background):
     else:
         window.configure(bg=Background)
     return window
-def add_button(window, text, command, X, Y):
-    button = tk.Button(window, text=text, command=command)
+
+def add_button(window, text, command, X, Y, color=None):
+    button = tk.Button(window, text=text, command=command, bg=color)
     button.place(x=X, y=Y)
     
-def add_label(window, text, X, Y):
-    label = tk.Label(window, text)
+def add_label(window, text, X, Y, color=None):
+    label = tk.Label(window, text, bg=color)
     label.place(x=X, y=Y)
 
-def add_textbox(window):
-    entry = tk.Entry(window)
+def add_textbox(window, color=None):
+    entry = tk.Entry(window, bg=color)
     entry.pack()
 
-def promptFileOpen(Title):
+def promptFileOpen(Title=None):
+    if Title == None:
+        Title = "File Menu"
     file_path = filedialog.askopenfilename(title=Title)
     return file_path
