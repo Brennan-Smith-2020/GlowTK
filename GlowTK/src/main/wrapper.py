8c990d55a004f0e45718e8547bc95cf0235764bf
AnimGUI <104740023+PackageCreator@users.noreply.github.com>
2023-08-10 02:06:29 -0400

    gets rid of test + more code fixes

--- GlowTK/src/main/wrapper.py
+++ GlowTK/src/main/wrapper.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from ttkthemes import ThemedTk
 from tkinter import filedialog
 import logging
+import os
 
 class Color:
     WHITE = "#ffffff"
@@ -38,6 +39,7 @@ def add_textbox(window, X, Y, color=None):
     entry.place(x=X, y=Y)  # Adjust the coordinates as needed
 
 
+global pathvar
 def promptFileOpen(Title=None):
     if Title is None:
         Title = "File Menu"
@@ -45,13 +47,3 @@ def promptFileOpen(Title=None):
     return file_path
 
 
-# Write here 
-
-
-window = create_window("daynes Portal", "200x400", "grey", "metro")
-label = add_label(window, "button = open file, textbox = type for fun :)", 200, 200, None, None, 12)
-textbox = add_textbox(window, 200, 100)
-button = add_button(window, "Click me", promptFileOpen, 100, 100, None)
-window.mainloop()
-
-"""im working on an upgraded GUI lib, with functions :)"""
