528cdf003cf6c4221e0efd8b157911476d583230
AnimGUI <104740023+PackageCreator@users.noreply.github.com>
2023-08-10 14:04:51 -0400

    Removed tests, added copyright law, added more colors in the class color

--- GlowTK/src/main/wrapper.py
+++ GlowTK/src/main/wrapper.py
@@ -5,10 +5,42 @@ from tkinter import filedialog
 import logging
 import os
 
+###
+# copyright PackageCreator 2023-2024. See license for more info.
+# Do not redistrubute the software or substancial portions of the "Software"
+# Without giving proper credit to PackageCreator.
+# Do not sell the software for any sort of monetary item, wheter it be trading,
+# Money, or currency of any sort.
+###
+
 class Color:
     WHITE = "#ffffff"
-    BLACK = "#0f0f0f"
-    GREY = "#615d53"
+    BLACK = "#000000"
+    GREY = "#808080"
+    RED = "#ff0000"
+    GREEN = "#00ff00"
+    BLUE = "#0000ff"
+    YELLOW = "#ffff00"
+    ORANGE = "#ffa500"
+    PURPLE = "#800080"
+    PINK = "#ffc0cb"
+    CYAN = "#00ffff"
+    MAGENTA = "#ff00ff"
+    BROWN = "#a52a2a"
+    LIME = "#00ff00"
+    INDIGO = "#4b0082"
+    TEAL = "#008080"
+    OLIVE = "#808000"
+    MAROON = "#800000"
+    NAVY = "#000080"
+    SILVER = "#c0c0c0"
+    GOLD = "#ffd700"
+    DARK_GREEN = "#006400"
+    DARK_RED = "#8b0000"
+    DARK_BLUE = "#00008b"
+    DARK_GREY = "#a9a9a9"
+    LIGHT_GREY = "#d3d3d3"
+
 
 
 def create_window(Title, Geometry, Background=None, Theme=None):
@@ -45,5 +77,3 @@ def promptFileOpen(Title=None):
         Title = "File Menu"
     file_path = filedialog.askopenfilename(title=Title)
     return file_path
-
-
